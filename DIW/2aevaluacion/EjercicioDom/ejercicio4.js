d507f45141937ee544f019bd461cf035cb1c1aac
Il Drago D'oro <vicen333@gmail.com>
2021-02-10 16:57:38 +0100

    idk

--- DIW/2aevaluacion/EjercicioDom/ejercicio4.js
+++ DIW/2aevaluacion/EjercicioDom/ejercicio4.js
@@ -4,9 +4,9 @@ window.onload = () => {
       $('body').find('.highlight').removeClass('highlight'); //Elimino cualquier highlight anterior
       var filtro = new RegExp(txt, 'ig'); //Creo un RegExp
       // Creo el string con el que reemplazar el texto
-      var str = "<span class='highlight'>" + txt + '</span>';
+      var str = "<span>" + txt + '</span>';
       if (txt != '') { //Si el input no está vacío:
-        $('body').each(function () { //Para cada elemento del body hace esto:
+        $('p').each(function () { //Para cada elemento del body hace esto:
           //Reemplaza cada elemento que coincida por un span con highlight
           $(this).html($(this).html().replace(filtro, str));
         });
