98fd25dfc4a488ece7a8b80954a9d9574da3e069
Il Drago D'oro <vicen333@gmail.com>
2021-02-10 10:10:58 +0100

    Finalizada actividad 4 de Diw: Text search highlight. By drago.

--- DIW/2aevaluacion/EjercicioDom/ejercicio4.js
+++ DIW/2aevaluacion/EjercicioDom/ejercicio4.js
@@ -1,5 +1,15 @@
-window.onload=()=>{
-  $('#pattern').on('keyup',function(){
-    
-  });
-}
+window.onload = () => {
+  $('#btn').click(function () { //Cojo el boton y le hago función al evento click
+      var txt = $('#pattern').val(); //Cojo el valor del input
+      $('body').find('.highlight').removeClass('highlight'); //Elimino cualquier highlight anterior
+      var filtro = new RegExp(txt, 'ig'); //Creo un RegExp
+      // Creo el string con el que reemplazar el texto
+      var str = "<span class='highlight'>" + txt + '</span>';
+      if (txt != '') { //Si el input no está vacío:
+        $('body').each(function () { //Para cada elemento del body hace esto:
+          //Reemplaza cada elemento que coincida por un span con highlight
+          $(this).html($(this).html().replace(filtro, str));
+        });
+      }
+    });
+};
