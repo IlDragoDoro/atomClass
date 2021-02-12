efa01ae691cbf22f9ae5a20cea8c7885b4c88860
Il Drago D'oro <vicen333@gmail.com>
2021-02-12 14:51:06 +0100

    DIW -> tablas creadas

--- DIW/2aevaluacion/EjercicioDom/ejercicio6.js
+++ DIW/2aevaluacion/EjercicioDom/ejercicio6.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
+$('#select').click(function(){
+  $(this).parents('tr').attr('style','background-color:yellow');
+});
 
+$('#delete').click(function(){
+  $('td').remove();
+});
+
+$('#update').click(function(){
+  $('#uno').val($('td').parents('#position'));
+  $('#dos').val('team');
+  $('#tres').val('50');
+});
 });
