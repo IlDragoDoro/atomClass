5731a72d646ece0f141adbc2d0007a35044747a8
Il Drago D'oro <vicen333@gmail.com>
2021-02-15 10:42:37 +0100

    Ejercicio 4 completado, faltan 1-3 y 5

--- DWC/RepasoExamen2/Funciones/ejercicio1.js
+++ DWC/RepasoExamen2/Funciones/ejercicio1.js
@@ -1,3 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
-  
+  var time = document.getElementById('time');
+  var st = document.getElementById('st');
+  var rt = document.getElementById('rt');
+  st.onclick = () => {
+    if (st.value == 'Start') {
+      start(time, st);
+    }else {
+      stop(time, st);
+    }
+  };
 });
+
+function start(time, st) {
+  st.value = 'Stop';
+  var ms = 00;
+  time.innerHTML = '00:00:' + ms;
+}
+
+function stop(time, st) {
+  st.value = 'Start';
+}
+
+function reestablecer(time, rt) {
+
+}
--- DWC/RepasoExamen2/Funciones/ejercicio4.js
+++ DWC/RepasoExamen2/Funciones/ejercicio4.js
@@ -1,3 +1,31 @@
 document.addEventListener('DOMContentLoaded', function () {
-  
+  var input = document.getElementById('input');
+  var btn = document.getElementById('button');
+  var time = document.getElementById('time');
+
+  btn.onclick = () => {
+    temporizador(input, btn, time);
+  };
 });
+
+function temporizador(input, btn, time) {
+  if (input.value > 0) {
+    document.getElementById('txt').innerHTML = '';
+    time.innerHTML = input.value;
+    var a = input.value;
+    var int = setInterval(function () {
+      if (a > 0) {
+        a -= 1;
+        time.innerHTML = a;
+      }else if (a == 0) {
+        alert('Se ha acabado el tiempo!!');
+        input.value = '';
+        clearInterval(int);
+      }
+    }, 1000);
+
+  }else {
+    document.getElementById('txt').innerHTML = 'El tiempo debe ser mayor a 0';
+    time.innerHTML = 0;
+  }
+}
